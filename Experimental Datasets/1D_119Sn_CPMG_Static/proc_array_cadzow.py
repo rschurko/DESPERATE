4f167cb7498f4d7c3f076684c48927f9d2e57dbc
adamaltenhof <ara19i@my.fsu.edu>
2021-11-01 23:11:47 -0400

    Plotting Changes

--- Experimental Datasets/1D_119Sn_CPMG_Static/proc_array_cadzow.py
+++ Experimental Datasets/1D_119Sn_CPMG_Static/proc_array_cadzow.py
@@ -3,41 +3,32 @@ import os
 sys.path.insert(0,'/Users/SRG/Documents/Adam/Python/SSNMR/functions')
 import numpy as np
 import functions as proc
-import urQRd1 as proc2
+import simpson as simproc
 import time
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from tabulate import tabulate
 start_time = time.time()
 
 ##Script will loop over array of Sn spectra
-def snrp(spec,i,j):
-    """Peak-to-peak SNR. Need to know the indicies of the max [i] and min[j] peaks."""
-    
-    spec = np.real(spec)
-    sn = np.abs( spec[i] - spec[j] ) / np.std(spec[0:100])
-    print('SNRp = %.3f' %sn)
-    return sn
-
-##Also used highest signal avg as SSIM 
-def ssim(specref, measure):
-    X = np.real( measure )
-    Y = np.real( specref )
-    SSIM = (2*np.mean(X)*np.mean(Y) + 0)*( 2*np.cov(X,Y)[0][1] + 0) /( (np.mean(X)**2 + np.mean(Y)**2 +0)*(np.std(X)**2 +np.std(Y)**2 + 0))
-    return SSIM
-
 ##Get High SNR ref spectrum for SSIM:
-gb = 16 #global gaussian broaden
+gb = 22 #global gaussian broaden
+zf = 0
 os.chdir( 'C:\\Users\\SRG\\Documents\\Adam\\Spectromter_Data\\SnO_May30_2021\\10' ) #1k scan EXP
 fid, SW = proc.loadfid('fid',plot='no')  
-cpmg, fidcoadd, dumspec = proc.coadd(fid,lb = 0, plot='no')
-freq = proc.freqaxis(fidcoadd,SW)
+cpmg, fidcoadd = proc.coadd(fid, plot='no')
+#cpmg, fidcoadd, dumspec = proc.coaddg(fid, plot='no')
 fidcoadd = fidcoadd[1:]  #Weird glitch, need to throw out first point to balance
 fidcoadd = proc.gauss(fidcoadd,gb)
-ph = [371.283066651318, 44952.976, -1690.231670275709, 99.559]
+ph = [367, 52161, -1636, 100]
+#ph = [367, 52161, -1636, 100]
 spec = proc.phase(proc.fft(fidcoadd),ph)
+#plt.plot(np.real(spec))
+#sys.exit()
 spec = np.real(spec)/np.max(np.real(spec))
-plt.plot(np.real(spec))
-sys.exit()
+freq = proc.freqaxis(spec)
+b = simproc.nearest(freq,-170.9)
+a= simproc.nearest(freq,24.38)
 ##Move on to main loop
 
 path = 'C:\\Users\\SRG\\Documents\\Adam\\Spectromter_Data\\SnO_May30_2021\\'
@@ -45,8 +36,8 @@ m = 8
 
 SSIMin =  np.zeros(m)
 SSIMout =  np.zeros(m)
-specrecon = np.zeros((1024,m),dtype='complex64')
-specin = np.zeros((1024,m),dtype='complex64')
+specrecon = np.zeros((len(spec),m),dtype='complex64')
+specin = np.zeros((len(spec),m),dtype='complex64')
 snrpin = np.zeros(m)
 snrpout =  np.zeros(m)
 for i in range(m):
@@ -55,31 +46,37 @@ for i in range(m):
     os.chdir( path + str(kk) )
     fid, SW = proc.loadfid('fid',plot='no')  
 
-    cpmg, fidcoadd, dumspec = proc.coadd(fid,lb = 0, plot='no') #save lb for cadzow
+    cpmg, fidcoadd = proc.coadd(fid, plot='no') #save lb for cadzow
     fidcoadd = fidcoadd[1:]
     
-    specintemp = proc.phase(proc.fft(proc.gauss(fidcoadd,gb)),ph)[0,:]
+    specintemp = proc.phase(proc.fft(proc.gauss(fidcoadd,gb)),ph)
     specin[:,i] = np.real(specintemp)/np.max(np.real(specintemp))
-    SSIMin[i] = ssim(spec,specin[:,i]) #SSIM of raw spec
-    snrpin[i] = snrp(specin[:,i] ,312,721)
+    SSIMin[i] = simproc.ssim(spec,specin[:,i]) #SSIM of raw spec
+    snrpin[i] = simproc.snrp(specin[:,i] ,a,b)
 
     #Denoise
-    fidrecon = proc.cadzow(fidcoadd, ph, SW,lb = gb)
+    fidrecon = proc.cadzow(fidcoadd, 10)
+    fidrecon = proc.gauss(fidrecon,gb)
     
-    plt.close()
-    
-    specrecont = proc.phase(proc.fft(fidrecon),ph)[0,:]
+    specrecont = proc.phase(proc.fft(fidrecon),ph)
     specrecon[:,i] = np.real(specrecont)/np.max(np.real(specrecont))
-    SSIMout[i] = ssim(spec,specrecon[:,i])
-    snrpout[i] = snrp(specrecon[:,i],312,721)
+    SSIMout[i] = simproc.ssim(spec,specrecon[:,i])
+    snrpout[i] = simproc.snrp(specrecon[:,i],a,b)
+
+c = SSIMin[0] - 1 #SSIMin of 1024 against itself has this difference from 1.0000
+
+SSIMin = SSIMin - c
+SSIMout = SSIMout - c
 
 plt.figure(1)
 for i in range(len(snrpin)):
     plt.plot(freq,np.real(specin[:,i]) - i*np.max(np.real(specin[:,i])), label = 'SNRpp_in = %.1f , SSIM_in = %.4f' % (snrpin[i],SSIMin[i]))
 plt.legend(loc='upper right')
-plt.title('Noisey Spectra')
+plt.title('Noisy Spectra')
 plt.xlabel('Frequency (kHz)')
-plt.gca().invert_xaxis()
+plt.xlim((100, -250))
+plt.minorticks_on()
+plt.yticks([])
 
 plt.figure(2)
 for i in range(len(snrpin)):
@@ -87,20 +84,18 @@ for i in range(len(snrpin)):
 plt.legend(loc='upper right')
 plt.title('Denoised Spectra')
 plt.xlabel('Frequency (kHz)')
-plt.gca().invert_xaxis()
+plt.xlim((100, -250))
+plt.minorticks_on()
+plt.yticks([])
 
-c = 0.00097758 #SSIMin of 1024 against itself has this difference from 1.0000
-
-SSIMin = SSIMin - c
-SSIMout = SSIMout - c
 
 ##Table SNR peak-peak
 ns = [1024, 512, 256, 128, 64, 32, 16, 8]
 data=[]
 for i in range(len(snrpin)):
-    data.append( ["%d"%ns[i],  "%.1f"%snrpin[i], "%.4f"%SSIMin[i], "%.1f"%snrpout[i], "%.4f"%SSIMout[i]] )
+    data.append( ["%d"%ns[i],  "%.1f"%snrpin[i], "%.1f"%snrpout[i], "%.4f"%SSIMin[i],  "%.4f"%SSIMout[i]] )
 # create header
-head = ['ns', 'SNRpp_in', 'SSIM_in', 'SNRpp_out', 'SSIM_out']
+head = ['ns', 'SNRpp_in', 'SNRpp_out', 'SSIM_in', 'SSIM_out']
 # display table
 print(tabulate(data, headers=head, tablefmt="pretty", floatfmt="5.4f"))
 #generate ns counter at end
--- simulations/sim_proc_Cadzow_bench.py
+++ simulations/sim_proc_Cadzow_bench.py
@@ -6,113 +6,98 @@ import functions as proc
 import urQRd1 as proc2
 import simpson as simproc
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import time
 from tabulate import tabulate
 start_time = time.time()
-
-def snrp(spec,i,j):
-    """Peak-to-peak SNR. Need to know the indicies of the max [i] and min[j] peaks."""
-    
-    spec = np.real(spec)
-    sn = ( spec[i] - spec[j] ) / np.std(spec[0:100])
-    print('SNRp = %.3f' %sn)
-    return sn
-
-def ssim(specref, measure):
-    X = np.real( measure )
-    Y = np.real( specref )
-    SSIM = (2*np.mean(X)*np.mean(Y) + 0)*( 2*np.cov(X,Y)[0][1] + 0) /( (np.mean(X)**2 + np.mean(Y)**2 +0)*(np.std(X)**2 +np.std(Y)**2 + 0))
-    return SSIM
 
 T2 = 0.1 #fake T2 global, unitless
 fid,SW = simproc.read('Sn_CPMG_id.fid', lb=T2, plot='no')
 
 ph = [558.67, 92160.09, 0, 0]
-
-gb = 10 ##Amount of global gaussian broadening for coadd
+gb = 16 ##Amount of global gaussian broadening for coadd
 
 fid = simproc.coadd(fid, 512, 50)
 fidref = proc.gauss( fid , lb = gb )
 specref = proc.phase(proc.fft(fidref),ph)
 
-kk = 11 ##Number of noisy data points
+freq = simproc.freqaxis(specref,1e6)
+a = simproc.nearest(freq,-146.7)
+b= simproc.nearest(freq,293.8)
 
-snrpin = np.zeros(kk)
-snrpout =  np.zeros(kk)
-snrin =  np.zeros(kk)
-snrout =  np.zeros(kk)
-SSIMin =  np.zeros(kk)
-SSIMout =  np.zeros(kk)
-res =  np.zeros(kk)
+kk = 8 ##Number of noisy data points
+
+snrpin = np.zeros(kk); snrpout =  np.zeros(kk); snrin =  np.zeros(kk)
+snrout =  np.zeros(kk);SSIMin =  np.zeros(kk) ; SSIMout =  np.zeros(kk); res =  np.zeros(kk)
 specrecon = np.zeros((len(specref),kk),dtype='complex64')
 specin = np.zeros((len(specref),kk),dtype='complex64')
 for i in range(kk):
     fid,SW = simproc.read('Sn_CPMG_id.fid', lb=T2, plot='no') #dup for coadd
     fid = simproc.noise(fid,(i+1)*0.1)
     fid = simproc.coadd(fid, 512, 50)                   #dup for coadd
-    fid = proc.gauss(fid , lb = gb)
+    fid2 = np.copy(fid) #dup for coadd
+    fid = proc.gauss(fid, lb = gb)
     spec = proc.phase(proc.fft(fid),ph)
     specin[:,i] = spec
     
-    #spec = proc.phase(spec,ph)[0,:]
-    snrpin[i] = snrp(spec,1446,3249) ##CHK index based on ZF
+    snrpin[i] = simproc.snrp(spec,a,b) 
     snrin[i] = proc.snr(spec,j=0)
+    SSIMin[i] = simproc.ssim(specref,spec) #problem here that spec is not GB'd
+
+    fidrecon = proc.cadzow(fid2, p=35) ##Slow Cadzow
+    fidrecon = proc.gauss(fidrecon,gb)
     
-    #fid,SW = simproc.read('Sn_CPMG_id.fid', lb=T2, plot='no') #dup for coadd
-    #fid = simproc.noise(fid,(i+1)*0.1)
-    #fid = proc.gauss(fid , lb = gb )
-    #spec = proc.phase(proc.fft(fid),ph)[0,:]
-    SSIMin[i] = ssim(specref,spec) #problem here that spec is not GB'd
-
-    fid,SW = simproc.read('Sn_CPMG_id.fid', lb=T2, plot='no') #dup again for cadzow
-    fid = simproc.noise(fid,(i+1)*0.1)
-    fid, spec = simproc.coadd(fid, 512, 50)                   #dup again for cadzow
-    #fid = proc.gauss(fid , lb = gb)
-
-    fidrecon = simproc.cadzow(fid,ph,SW,lb = gb) ##Slow Cadzow
-    plt.close()
     specrecon[:,i] = proc.phase(proc.fft(fidrecon),ph)
-    snrpout[i] = snrp(specrecon[:,i],1446,3249)
-
-    SSIMout[i], res[i], snrout[i] = simproc.residual(fidref,fidrecon,SW,ph,plot='no')
+    snrpout[i] = simproc.snrp(specrecon[:,i],a,b)
+    SSIMout[i] = simproc.ssim(specref,specrecon[:,i])
     
-    del fid
-    del fidrecon
-    #del spec
-    #del specrecon
-#specrecon = simproc.coadd(fidrecon,512,50)
-#plt.plot(np.abs(specrecon))
-plt.close()
+
+c = SSIMin[0] - 1
+SSIMin -= c; SSIMout -= c
+
+######Plotting Stuff
+mpl.rcParams['font.family'] = "arial"
+mpl.rcParams['font.size'] = 14
 
 plt.figure(1)
 for i in range(len(snrin)):
-    plt.plot(np.real(specin[:,i]) - i*np.max(np.real(specin[:,i])), label = 'SNRpp_in = %.1f , SSIM_in = %.4f' % (snrpin[i],SSIMin[i]))
+    plt.plot(freq,np.real(np.flipud(specin[:,i])) - i*np.max(np.real(specin[:,i])), label = 'SNRpp_in = %.1f , SSIM_in = %.4f' % (snrpin[i],SSIMin[i]))
 plt.legend(loc='upper right')
-plt.title('Noisey Spectra')
+plt.title('Noisy Spectra')
+plt.xlabel('Frequency (kHz)')
+plt.xlim((250, -400))
+plt.minorticks_on()
+plt.yticks([])
 
 plt.figure(2)
 for i in range(len(snrin)):
-    plt.plot(np.real(specrecon[:,i]) - i*np.max(np.real(specrecon[:,i])), label = 'SNRpp_out = %.1f , SSIM_out = %.4f' % (snrpout[i],SSIMout[i]))
+    plt.plot(freq,np.real(np.flipud(specrecon[:,i])) - i*np.max(np.real(specrecon[:,i])), label = 'SNRpp_out = %.1f , SSIM_out = %.4f' % (snrpout[i],SSIMout[i]))
 plt.legend(loc='upper right')
 plt.title('Denoised Spectra')
-
-##Table SNR
-data=[]
-for i in range(len(snrin)):
-    data.append( ["%.1f"%snrin[i], "%.4f"%SSIMin[i], "%.1f"%snrout[i], "%.4f"%SSIMout[i]] )
-# create header
-head = ['SNR_in','SSIM_in', 'SNR_out', 'SSIM_out']
-# display table
-print(tabulate(data, headers=head, tablefmt="pretty", floatfmt="5.4f"))
+plt.xlabel('Frequency (kHz)')
+plt.xlim((250, -400))
+plt.minorticks_on()
+plt.yticks([])
 
 ##Table SNR peak-peak
 data=[]
 for i in range(len(snrin)):
-    data.append( ["%.1f"%snrpin[i], "%.4f"%SSIMin[i], "%.1f"%snrpout[i], "%.4f"%SSIMout[i]] )
+    data.append( ["%.1f"%snrpin[i], "%.1f"%snrpout[i], "%.4f"%SSIMin[i],  "%.4f"%SSIMout[i]] )
 # create header
-head = ['SNRpp_in', 'SSIM_in', 'SNRpp_out', 'SSIM_out']
+head = ['SNRpp_in', 'SNRpp_out', 'SSIM_in', 'SSIM_out']
 # display table
 print(tabulate(data, headers=head, tablefmt="pretty", floatfmt="5.4f"))
+
+##Table SNR
+# data=[]
+# for i in range(len(snrin)):
+#     data.append( ["%.1f"%snrin[i], "%.4f"%SSIMin[i], "%.1f"%snrout[i], "%.4f"%SSIMout[i]] )
+# # create header
+# head = ['SNR_in','SSIM_in', 'SNR_out', 'SSIM_out']
+# # display table
+# print(tabulate(data, headers=head, tablefmt="pretty", floatfmt="5.4f"))
+
+
 
 print('Finished!')
 print("-- %5.5f s Run Time --" % (time.time() - start_time))
